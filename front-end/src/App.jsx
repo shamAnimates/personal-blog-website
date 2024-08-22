94c6c4fccabfce47b324ff141dfcf657131e1edc
shamAnimates <145093437+shamAnimates@users.noreply.github.com>
2024-08-22 23:07:21 +0530

    first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div class="text-center text-blue-500">
+  <h1 class="text-3xl font-bold">
+    Hello, Tailwind!
+  </h1>
+</div>
+
+  )
+}
+
+export default App ;
